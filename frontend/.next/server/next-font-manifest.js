451ebfc4d5799b2c5da7dc36411b4cf834336dfc
Jeffkroon <jeffkroon03@icloud.com>
2025-10-02 10:58:37 +0200

    🧹 Clean up git tracking
    - Remove build artifacts (.next/, node_modules/) from git tracking
    - These files are already in .gitignore but were previously tracked
    - Now they will be properly ignored in future changes

--- frontend/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/jeffkroon/localdocuments/backend_chatbot_dashboard/frontend/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
